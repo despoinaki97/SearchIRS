bfa9071e23783bda6ea8453579e936bd07368a3f
despoinaki97 <despoinaki97@gmail.com>
2019-04-24 00:27:35 +0300

    On progress getting the docs from posting

--- SearchIRS/src/main/java/SearchWord.java
+++ SearchIRS/src/main/java/SearchWord.java
@@ -1,4 +1,7 @@
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
@@ -15,6 +18,7 @@ import java.util.Scanner;
 public class SearchWord {
 
     public static String Sword;
+    public static ArrayList<String> foundfiles;
 
     public static String inputword() {
         Scanner user_input = new Scanner(System.in);
@@ -32,4 +36,20 @@ public class SearchWord {
             return index;
         }
     }
+
+    public static ArrayList<String> searchfiles(int index) throws IOException {
+        int i = 0;
+        ArrayList<String> docs = new ArrayList<>();
+        long offsone = LoadVocab.offset.get(index);
+        int df = LoadVocab.df.get(index);
+        long offstwo = LoadVocab.offset.get(index++);
+        RandomAccessFile posting = new RandomAccessFile("PostingFile.txt", "r");
+        posting.seek(offsone);
+        String line;
+        for (i = 0; i < df; i++) {
+            line = posting.readLine();
+        }
+        posting.seek(offstwo);
+    }
+
 }
